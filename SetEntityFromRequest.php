cf232d2368ee4421c1ac53cb3b4c4cf545aa34bc
hlc0110 <474333983@qq.com>
2023-06-23 21:21:08 +0800

    Update SetEntityFromRequest.php

--- SetEntityFromRequest.php
+++ SetEntityFromRequest.php
@@ -1,7 +1,8 @@
 <?php
+
 /**
  * 一个简单的请求参数绑定到类的属性上
- * 如果请求参数过多，手动挨个接收代码会很长
+ * 如果请求参数过多，挨个获取有点累
  * 注意：实体类的方法名是标准的getXxx or setXxx
  */
 class RequestParamsBindManager{
@@ -9,48 +10,68 @@ class RequestParamsBindManager{
     private static $reflection;
 
     /**
-     * @throws ReflectionException
+     * 方式一
+     * @throws Exception
+     * @desc $obj = RequestParamsBindManager::bindFromObject(new Abc())
      */
-    public static function bind($bindObject){
-        self::$reflection = new ReflectionClass($bindObject);
-        self::$bindObject = $bindObject;
-
-        $attrs = self::$reflection->getProperties();        // ReflectionProperty 数组
-        foreach ($attrs as $attr){
-            $requestName = $attr->getName();
-            $setAttrMethod = 'set' . ucfirst($requestName);
-            if(isset($_REQUEST[$requestName]) && self::$reflection->hasMethod($setAttrMethod)){
-                $method = self::$reflection->getMethod($setAttrMethod);
-                $method->invoke(self::$bindObject, self::getReqValue($requestName));
-            }
+    public static function bindFromObject($bindObject){
+        try{
+            self::$reflection = new ReflectionClass($bindObject);
+            self::$bindObject = $bindObject;
+            self::bind();
+            return self::$bindObject;
+        }catch (ReflectionException $exception){
+            throw new Exception("解析类异常");
         }
-        return self::$bindObject;
     }
+
+    /**
+     * 方式二
+     * @desc $obj = RequestParamsBindManager::bingFromClass(Abc::class)
+     * @throws Exception
+     */
+    public static function bingFromClass($class){
+        try {
+            self::$reflection = new ReflectionClass($class);
+            self::$bindObject = self::$reflection->newInstance();
+            self::bind();
+            return self::$bindObject;
+        }catch (ReflectionException $exception){
+            throw new Exception("解析类异常");
+        }
+    }
+
     // 这里可以做一些过滤
     public static function getReqValue($name){
-        return $_REQUEST($name);
+        return $_REQUEST[$name];
+    }
+
+    protected static function bind(){
+        try{
+            $attrs = self::$reflection->getProperties();        // ReflectionProperty 数组
+            foreach ($attrs as $attr){
+                $requestName = $attr->getName();
+                $setAttrMethod = 'set' . ucfirst($requestName);
+                if(isset($_REQUEST[$requestName]) && self::$reflection->hasMethod($setAttrMethod)){
+                    $method = self::$reflection->getMethod($setAttrMethod);
+                    $method->invoke(self::$bindObject, self::getReqValue($requestName));
+                }
+            }
+        }catch (ReflectionException $exception){
+            throw new Exception("解析绑定request params失败");
+        }
     }
 }
 
 
 
-/** 以下是业务代码 **/
-
-/**
- * 请求参数实体类,需要接收的参数，定义成类的属性，提供set方法
- */
-class RequestParmas{
-    private $uid;
+class CrmSearchParams{
     private $nickname;
-
-    public function getUid(){
-        return $this->uid;
-    }
-
-    public function setUid($uid){
-        $this->uid = $uid;
-        return $this;
-    }
+    private $age;
+    private $gender;
+    private $uid;
+    private $note;
+    private $addr;
 
     public function getNickname(){
         return $this->nickname;
@@ -60,13 +81,16 @@ class RequestParmas{
         $this->nickname = $nickname;
         return $this;
     }
+
+    private function setAge($age){$this->age = $age;}
+    // .... 省略其他字段的get set方法
+
 }
 
-// 接收参数
-$requestParmas = RequestParamsBindManager::bind(new RequestParmas());
+$_REQUEST['nickname'] = "php开发";
+$_REQUEST['age'] = 18;
+$obj1 = RequestParamsBindManager::bindFromObject(new CrmSearchParams());
+print_r($obj1);
 
-print_r($requestParmas);
-
-// 使用接收的参数
-$requestParmas->getUid();
-?>
+$obj2 = RequestParamsBindManager::bingFromClass(CrmSearchParams::class);
+print_r($obj2);
